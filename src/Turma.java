19064e11bb0c81afc3816067fd8176a199592d5e
Luiz Fernando <fernandoluizalves24@gmail.com>
2026-05-21 10:19:21 -0300

    atualizando classes

--- src/Turma.java
+++ src/Turma.java
@@ -4,6 +4,23 @@ public class Turma{
     private Disciplina[] disciplinas;
     private Matricula[] matriculas;
 
+    //construtor
+    private Turma(int ano, int vagas) {
+        this.ano = ano;
+        this.vagas = vagas;
+        disciplinas = new Disciplina[vagas];
+        matriculas = new Matricula[vagas];
+    }
+
+    //metodo de fabrica
+    public static Turma getInstance(int ano, int vagas) {
+        if (ano > 2000 && vagas > 0 ) {
+            return new Turma(ano, vagas);
+        } else {
+            return null;
+        }
+    }
+
     public boolean adiciona(Matricula mat) {
         for (int i = 0; i < matriculas.length; i++) {
             if (matriculas[i] == null) {
@@ -31,6 +48,16 @@ public class Turma{
     public Matricula[] getMatriculas() {
         return matriculas;
     }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public int getVagas() {
+        return vagas;
+    }
+
+    
 
     
 }
